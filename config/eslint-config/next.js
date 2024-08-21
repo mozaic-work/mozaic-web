7c7ab26b37ed7cecc676cc95afad8a3ca84d33ba
fgouvea10 <felipegalvesouvea@gmail.com>
2024-08-21 20:21:11 -0300

    chore(auth): add nextjs project for auth webapp

--- config/eslint-config/next.js
+++ config/eslint-config/next.js
@@ -1,4 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   extends: ['@vtal/eslint-config/react'],
   plugins: ['simple-import-sort'],
